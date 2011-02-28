ddc8cd863dc47790050d6a626ab12ab64e485f21
to4kaprogrammingteam <to4kaprogrammingteam@9f6bd7e5-d0eb-13ab-02db-268927fcf40a>
2011-02-28 23:08:23 +0000

    Добавлен стандартный словарь и его инициализация

---
+++ trunk/system/StandartDictionary.php
@@ -0,0 +1,49 @@
+<?php
+/* 
+ * Стандартный словарь Форт-машины, инициализация
+ *
+ */
+
+namespace FORTH\SYSTEM;
+
+class StandartDictionary {
+
+	/**
+	 * Инициализация стандартного словаря
+	 */
+	public function init() {
+
+		$dict = Dictionary::getInstance();
+
+		/*
+		 * Печать верхнего числа со стека
+		 */
+		$dict->addWord(
+			new Word(
+				'.',
+				1,
+				1,
+				function ($a) {
+					echo $a;
+					return null;
+				}
+			)
+		);
+
+		/*
+		 * Сложение двух верхних чисел, помещение результата на стек
+		 */
+		$dict->addWord(
+			new Word(
+				'+',
+				2,
+				2,
+				function ($a, $b) {
+					return (array)($a+$b);
+				}
+			)
+		);
+
+	}
+	
+}
