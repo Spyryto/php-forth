0ca0ed0fd4c6485ea11b036ec15f103c5f781dd6
to4kaprogrammingteam <to4kaprogrammingteam@9f6bd7e5-d0eb-13ab-02db-268927fcf40a>
2011-03-01 01:01:37 +0000

    Доведение стандартного словаря до необходимого минимума, тестирование

--- system/StandartDictionary.php
+++ system/StandartDictionary.php
@@ -24,8 +24,36 @@ class StandartDictionary {
 				1,
 				1,
 				function ($a) {
-					echo $a;
+					echo $a . "\n\r";
 					return null;
+				}
+			)
+		);
+
+		/*
+		 * Дубль верхнего числа со стека
+		 */
+		$dict->addWord(
+			new Word(
+				'DUP',
+				1,
+				1,
+				function ($a) {
+					return array($a, $a);
+				}
+			)
+		);
+
+		/*
+		 * Обмен двух верхних чисел со стека
+		 */
+		$dict->addWord(
+			new Word(
+				'SWAP',
+				2,
+				2,
+				function ($a, $b) {
+					return array($a, $b);
 				}
 			)
 		);
@@ -39,7 +67,49 @@ class StandartDictionary {
 				2,
 				2,
 				function ($a, $b) {
-					return (array)($a+$b);
+					return (array) ($b+$a);
+				}
+			)
+		);
+
+		/*
+		 * Вычитание двух верхних чисел, помещение результата на стек
+		 */
+		$dict->addWord(
+			new Word(
+				'-',
+				2,
+				2,
+				function ($a, $b) {
+					return (array) ($b-$a);
+				}
+			)
+		);
+
+		/*
+		 * Умножение двух верхних чисел, помещение результата на стек
+		 */
+		$dict->addWord(
+			new Word(
+				'*',
+				2,
+				2,
+				function ($a, $b) {
+					return (array) ($b*$a);
+				}
+			)
+		);
+
+		/*
+		 * Целочисленное деление двух верхних чисел, помещение результата на стек
+		 */
+		$dict->addWord(
+			new Word(
+				'/',
+				2,
+				2,
+				function ($a, $b) {
+					return (array) (intval(floor($b/$a)));
 				}
 			)
 		);
